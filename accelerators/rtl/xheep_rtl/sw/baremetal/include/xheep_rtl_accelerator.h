18a139b0fac98171ef589610f973c90b6e24c145
FrancescoPoluzzi <francesco.poluzzi@epfl.ch>
2026-02-27 16:18:42 -0500

    make xheep_rtl_accelerator.h more solid

--- accelerators/rtl/xheep_rtl/sw/baremetal/include/xheep_rtl_accelerator.h
+++ accelerators/rtl/xheep_rtl/sw/baremetal/include/xheep_rtl_accelerator.h
@@ -17,6 +17,13 @@
 #define XHEEP_USR_BASE               0x40
 
 typedef struct {
+    uint32_t code_size_words;
+    uint32_t fetch_addr;
+    uint32_t fetch_trigger;
+    uint32_t exit_trigger;
+} xheep_conf_regs_t;
+
+typedef struct {
     uint32_t addr;
     uint32_t size;
     const uint8_t *data;
@@ -46,17 +53,40 @@ static inline void xheep_fw_flatten(uint8_t *buffer, size_t buffer_size,
     }
 }
 
+static inline xheep_conf_regs_t xheep_conf_regs_zero(void)
+{
+    xheep_conf_regs_t conf = {0};
+    return conf;
+}
+
 static inline void xheep_program_conf_regs(struct esp_device *dev,
-                                           const uint32_t conf_regs[XHEEP_NUM_CONFIG_REGS],
+                                           const xheep_conf_regs_t *conf,
                                            bool is_producer)
 {
     const uintptr_t reg_code_size = is_producer ? (XHEEP_USR_BASE + 4) : XHEEP_USR_BASE;
     const uintptr_t reg_fetch_addr = is_producer ? XHEEP_USR_BASE : (XHEEP_USR_BASE + 4);
+    const xheep_conf_regs_t safe_conf = (conf != NULL) ? *conf : xheep_conf_regs_zero();
 
-    iowrite32(dev, reg_code_size,  conf_regs[XHEEP_CONF_IDX_CODE_SIZE]);
-    iowrite32(dev, reg_fetch_addr, conf_regs[XHEEP_CONF_IDX_FETCH_ADDR]);
-    iowrite32(dev, XHEEP_USR_BASE + 8,  conf_regs[XHEEP_CONF_IDX_FETCH_TRIGGER]);
-    iowrite32(dev, XHEEP_USR_BASE + 12, conf_regs[XHEEP_CONF_IDX_EXIT_TRIGGER]);
+    iowrite32(dev, reg_code_size,  safe_conf.code_size_words);
+    iowrite32(dev, reg_fetch_addr, safe_conf.fetch_addr);
+    iowrite32(dev, XHEEP_USR_BASE + 8,  safe_conf.fetch_trigger);
+    iowrite32(dev, XHEEP_USR_BASE + 12, safe_conf.exit_trigger);
+}
+
+static inline void xheep_program_conf_all(struct esp_device *dev,
+                                          uint32_t code_size_words,
+                                          uint32_t fetch_addr,
+                                          uint32_t fetch_trigger,
+                                          uint32_t exit_trigger,
+                                          bool is_producer)
+{
+    const xheep_conf_regs_t conf = {
+        .code_size_words = code_size_words,
+        .fetch_addr = fetch_addr,
+        .fetch_trigger = fetch_trigger,
+        .exit_trigger = exit_trigger
+    };
+    xheep_program_conf_regs(dev, &conf, is_producer);
 }
 
 static inline void xheep_start(struct esp_device *dev)
@@ -86,26 +116,23 @@ static inline bool xheep_fetch_firmware(struct esp_device *dev, uint32_t code_si
                                         uint32_t fetch_addr, bool is_producer,
                                         unsigned max_polls)
 {
-    uint32_t conf_regs[XHEEP_NUM_CONFIG_REGS];
-    conf_regs[XHEEP_CONF_IDX_CODE_SIZE]     = code_size_words;
-    conf_regs[XHEEP_CONF_IDX_FETCH_ADDR]    = fetch_addr;
-    conf_regs[XHEEP_CONF_IDX_FETCH_TRIGGER] = 1;
-    conf_regs[XHEEP_CONF_IDX_EXIT_TRIGGER]  = 0;
-
-    xheep_program_conf_regs(dev, conf_regs, is_producer);
+    xheep_program_conf_all(dev, code_size_words, fetch_addr, 1, 0, is_producer);
     xheep_start(dev);
-    return xheep_wait_done(dev, max_polls, NULL);
+    unsigned polls = 0;
+    bool done = xheep_wait_done(dev, max_polls, &polls);
+    if (!done) {
+        unsigned status = ioread32(dev, STATUS_REG);
+        printf("Error: X-HEEP firmware fetch timeout status=0x%08x\n", status);
+    }
+    printf("[XHEEP] firmware fetch: done polls=%u\n", polls);
+    return done;
 }
 
 static inline void xheep_program_start(struct esp_device *dev, bool is_producer)
 {
-    uint32_t conf_regs[XHEEP_NUM_CONFIG_REGS];
-    conf_regs[XHEEP_CONF_IDX_CODE_SIZE]     = 0;
-    conf_regs[XHEEP_CONF_IDX_FETCH_ADDR]    = 0;
-    conf_regs[XHEEP_CONF_IDX_FETCH_TRIGGER] = 0;
-    conf_regs[XHEEP_CONF_IDX_EXIT_TRIGGER]  = 1;
+    xheep_program_conf_all(dev, 0, 0, 0, 0, is_producer);
 
-    xheep_program_conf_regs(dev, conf_regs, is_producer);
+    xheep_program_conf_all(dev, 0, 0, 0, 1, is_producer);
 }
 
 #endif
